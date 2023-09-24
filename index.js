0c15e905104a69c3c0a3b6bc33fa3d4dd1703360
ridho-fs <145777144+ridho-fs@users.noreply.github.com>
2023-09-24 22:05:46 +0700

    memorycard

---
+++ index.js
@@ -0,0 +1,86 @@
+const card = document.querySelectorAll('.cell')
+const front = document.querySelectorAll('.front')
+const container = document.querySelector('.container')
+const score = document.querySelector('.score span')
+const win = document.getElementById('you-won')
+
+suffleImage()
+clicking()
+
+function suffleImage(){
+    card.forEach(c=>{
+
+        const num = [...Array(card.length).keys()]
+        const random = Math.floor(Math.random()*card.length)
+
+        c.style.order = num[random]
+    })
+}
+
+function clicking(){
+
+    for(let i =0; i<card.length; i++){
+        front[i].classList.add('show')
+
+        setInterval(() => {
+            front[i].classList.remove('show')
+        }, 1000);
+
+        card[i].addEventListener('click' ,()=>{
+
+            front[i].classList.add('flip')
+           const filppedCard = document.querySelectorAll('.flip')
+
+            if(filppedCard.length == 2){
+
+                container.style.pointerEvents ='none'
+                
+                setInterval(() => {
+                    
+                    container.style.pointerEvents ='all'
+                }, 1000);
+ 
+                match(filppedCard[0] , filppedCard[1])
+
+                const openedCards = document.querySelectorAll('.match')
+                if (openedCards.length === card.length) {
+                    checkWin()
+                }
+            }
+        });
+    }
+}
+
+function match(cardOne , cardTwo){
+
+    if(cardOne.dataset.index == cardTwo.dataset.index){
+
+        score.innerHTML = parseInt(score.innerHTML) + 1
+       
+        cardOne.classList.remove('flip')
+        cardTwo.classList.remove('flip')
+
+
+        cardOne.classList.add('match')
+        cardTwo.classList.add('match')
+
+
+    }else{
+
+        setTimeout(() => {
+            
+            cardOne.classList.remove('flip')
+            cardTwo.classList.remove('flip')
+        }, 600);
+    }
+}
+
+function checkWin() {
+    win.style.display = 'block';
+
+    card.forEach(c => {
+        if (c.querySelector('.front').classList.contains('opened')) {
+            c.querySelector('.front').classList.remove('opened');
+        }
+    });
+}
